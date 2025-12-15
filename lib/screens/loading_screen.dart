430793a84d865884bc4808911e53a697518b2dc0
PradermN <pradermnafack@gmail.com>
2025-12-15 23:55:57 +0100

    Initialize gettting geolocation

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -7,13 +8,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    // Check permission status
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    // Request permission if not granted
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Location permission denied');
+        return;
+      }
+    }
+
+    // If permission is permanently denied, show error
+    if (permission == LocationPermission.deniedForever) {
+      print('Location permission permanently denied');
+      return;
+    }
+
+    // Get the position
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(accuracy: LocationAccuracy.low),
+    );
+    print(position);
+    print(position.latitude);
+    print(position.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
